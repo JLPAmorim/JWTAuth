5cc3e0865d13b73c597ccbbe4937f115ce2db782
João Amorim <JLPAmorim>
2021-06-28 21:05:16 +0100

    Dockerfile

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var LocalStrategy = require('passport-local').Strategy
 
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/Tarefas2021', 
+mongoose.connect('mongodb://db_auth:27017/Users', 
       { useNewUrlParser: true,
         useUnifiedTopology: true,
         serverSelectionTimeoutMS: 5000});
