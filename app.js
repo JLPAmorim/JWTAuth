8632b4f1abb2e1a8b515165be82d1f402593731e
João Amorim <JLPAmorim>
2021-06-28 23:51:59 +0100

    Changed mongo connection

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var LocalStrategy = require('passport-local').Strategy
 
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://db_auth:27017/Users', 
+mongoose.connect('mongodb://db_auth:27017/users', 
       { useNewUrlParser: true,
         useUnifiedTopology: true,
         serverSelectionTimeoutMS: 5000});
